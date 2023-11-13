c7c405fbeffd037e431a253b163691ca7b7200d5
iKOOLCORE <112603609+KoolCore@users.noreply.github.com>
2023-11-13 21:47:55 +0800

    Update passthrough.sh

--- Scripts/passthrough.sh
+++ Scripts/passthrough.sh
@@ -4,34 +4,38 @@
 cpu_vendor=$(lscpu | grep "Vendor ID" | awk '{print $3}')
 
 # 判断 CPU 型号并设置相应的参数
-if [ "$cpu_vendor" == "GenuineIntel" ]; then
-    echo "Detected Intel CPU."
+cpu_platform="$(lscpu | grep 'Model name' | grep -E 'Intel|AMD')"
+case $cpu_platform in
+    *Intel*)
+          CPU="Intel"
+          echo "侦测到本平台为 Intel CPU."
     # 如果是 Intel CPU
-    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt"/' /etc/default/grub
-elif [ "$cpu_vendor" == "AuthenticAMD" ]; then
-    echo "Detected AMD CPU."
+          sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt"/' /etc/default/grub
+          ;;
+    *AMD*)
+          CPU="AMD"
+          echo "侦测到本平台为 AMD CPU."
     # 如果是 AMD CPU
-    sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on iommu=pt"/' /etc/default/grub
-    # 如果需要显卡直通，添加额外的参数
-    # sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on iommu=pt video=vesafb:off video=efifb:off video=simplefb:off"/' /etc/default/grub
-else
-    echo "Unsupported CPU vendor: $cpu_vendor"
-    exit 1
-fi
+          sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on iommu=pt"/' /etc/default/grub
+          ;;
+    *)
+          echo -e "抱歉，暂不支持当前CPU平台"
+          ;;
+esac
 
 # 更新 grub
 echo "Updating GRUB..."
 update-grub
 
 # 加载内核模块
-echo "Loading kernel modules..."
+echo "加载内核模块..."
 echo "vfio" >> /etc/modules
 echo "vfio_iommu_type1" >> /etc/modules
 echo "vfio_pci" >> /etc/modules
 echo "vfio_virqfd" >> /etc/modules
 
 # 更新内核参数
-echo "Updating kernel parameters..."
+echo "更新内核参数..."
 update-initramfs -k all -u
 
-echo "Script completed successfully."
+echo "脚本运行完成，已成功在本机开启硬件直通功能."
