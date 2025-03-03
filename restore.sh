7b8da740f22f8594ab5a1ceeab0884ef22db227f
iKOOLCORE <112603609+KoolCore@users.noreply.github.com>
2025-03-03 08:40:36 +0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,4 +3,4 @@ sed -i '/PVE::pvecfg::version_text();/,/my $dinfo = df/!b;//!d;s/my $dinfo = df/
 sed -i '/pveversion/,/^\s\+],/!b;//!d;s/^\s\+],/\t    value: '"'"''"'"',\n\t},\n&/' /usr/share/pve-manager/js/pvemanagerlib.js
 sed -i '/widget.pveNodeStatus/,/},/ { s/height: [0-9]\+/height: 300/; /textAlign/d}' /usr/share/pve-manager/js/pvemanagerlib.js
 systemctl restart pveproxy
-echo "已还原成功，请使用Shift + F5 强制刷新网页！"
+echo -e "已还原成功，请使用Shift + F5 强制刷新网页！"
