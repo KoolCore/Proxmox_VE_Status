e3a77474a3bcb4b96ef1d6b516e9a4f0dfe21395
iKOOLCORE <112603609+KoolCore@users.noreply.github.com>
2025-03-03 08:37:48 +0800

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sed -i '/PVE::pvecfg::version_text();/,/my $dinfo = df/!b;//!d;s/my $dinfo = df/\n\t&/' /usr/share/perl5/PVE/API2/Nodes.pm
+sed -i '/pveversion/,/^\s\+],/!b;//!d;s/^\s\+],/\t    value: '"'"''"'"',\n\t},\n&/' /usr/share/pve-manager/js/pvemanagerlib.js
+sed -i '/widget.pveNodeStatus/,/},/ { s/height: [0-9]\+/height: 300/; /textAlign/d}' /usr/share/pve-manager/js/pvemanagerlib.js
+systemctl restart pveproxy
